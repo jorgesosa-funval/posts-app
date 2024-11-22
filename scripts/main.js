633eca8a0659cbaeb6e2d2f534cd62a45dd79071
Jorge Sosa <jorgesosa@fundaciondevalores.org>
2024-11-22 10:01:17 -0400

    change card mobile styles

--- scripts/main.js
+++ scripts/main.js
@@ -40,9 +40,9 @@ function loadPosts() {
 function gerateTemplate({titulo,fecha}) {
     const post_template = `
         <li class="list-group-item d-md-flex align-items-center justify-content-between">
-            <div class="d-flex align-items-center justify-content-between flex-grow-1 info ">
-               <h3>${titulo}</h3>
-               <h6>${fecha}</h6>
+            <div class="d-flex align-items-md-center justify-content-between flex-grow-1 info flex-column-reverse flex-md-row">
+               <h3 class="m-0">${titulo}</h3>
+               <h6 class="m-0 text-end fst-italic">${fecha}</h6>
             </div>
             <div class="action d-flex justify-content-end gap-3 flex-grow-1">
                <button class="btn bg-primary">
